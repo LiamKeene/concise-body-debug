3e070c849fe1ae413d7edb8ca5a017afd3c90906
Liam Keene <liam.keene@sentia.com.au>
2017-12-12 11:41:14 +1100

    Initial commit

---
+++ spec/concise-body-debug-view-spec.js
@@ -0,0 +1,9 @@
+"use babel"
+
+import ConciseBodyDebugView from "../lib/concise-body-debug-view"
+
+describe("ConciseBodyDebugView", () => {
+  it("has one valid test", () => {
+    expect("life").toBe("easy")
+  })
+})
